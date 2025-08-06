f8ad7d7d83efc40594608d8452da8e5cd7677b05
Abel <girisha.sangih@gmail.com>
2025-08-06 18:40:26 -0300

    Se inicializo un nuevo proyecto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Autores from './componentes/Autores'
+// import Libros from './componentes/Libros'
+
+function App(){
+  return(
+    <>
+    <div className='App'>
+      <Autores/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/componentes/Libros.jsx
@@ -0,0 +1,16 @@
+import {use, useState} from 'react';
+export default function Libros(){
+    const [titulo, setTitulo] = useState('');
+    const [isvn, setIsvn] = useState('');
+    const [genero, setGenero] = useState('');
+    const [sinopsis, setSinopsis] = useState('');
+    const [autorId, setAutorId] = useState('');
+
+    return(
+        <div className='Panel'>
+           <form>
+            
+           </form> 
+        </div>
+    )
+}
